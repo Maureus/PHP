f37597f0d72247ad8054cb1e44f93e717b0c9524
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2020-12-04 18:14:33 +0100

    Added registration validation

--- knokledge-db-new/resources/js/components/Register.vue
+++ knokledge-db-new/resources/js/components/Register.vue
@@ -1,57 +1,71 @@
 <template>
     <div class="flex flex-wrap w-full justify-center items-center pt-56">
         <div class="flex flex-wrap max-w-xl">
-            <div class="p-2 text-2xl text-white font-semibold"><h1>Register an account</h1></div>
-            <div class="p-2 w-full">
-                <label class="w-full text-white" for="name">Name:</label>
-                <span class="w-full text-red-500" v-if="errors.name">{{errors.name[0]}}</span>
-                <input id="name" class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Name" type="text" v-model="form.name" >
-            </div>
-            <div class="p-2 w-full">
-                <label class="text-white" for="email">Your e-mail:</label>
-                <input id="email" class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Email" type="email" v-model="form.email">
-            </div>
-            <div class="p-2 w-full">
-                <label class="text-white" for="password">Password:</label>
-                <input id="password" class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Password" type="password" v-model="form.password" name="password">
-            </div>
-            <div class="p-2 w-full">
-                <label class="text-white" for="confirm_password">Confirm Password:</label>
-                <input id="confirm_password" class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Confirm Password" type="password" v-model="form.password_confirmation" name="password_confirmation">
-            </div>
-            <div class="p-2 w-full mt-4">
-                <button @click.prevent="saveForm" type="submit" class="flex text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Register</button>
+            <div class="form-container">
+                <div class="p-2 text-2xl text-white font-semibold">
+                    <h1>Register an account</h1>
+                </div>
+                <form @submit.prevent="saveForm">
+                    <div class="p-2 w-full">
+                        <label class="w-full text-white" for="name">Name:</label>
+                        <span class="w-full text-red-500" v-if="errors.name">{{ errors.name[0] }}</span>
+                        <input id="name" placeholder="Name" type="text" v-model="form.name" required
+                               class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2"/>
+                    </div>
+                    <div class="p-2 w-full">
+                        <label class="text-white" for="email">Your e-mail:</label>
+                        <input id="email" placeholder="Email" type="email" v-model="form.email" required
+                               pattern="^[a-zA-Z][a-zA-Z0-9!#$%&'*+-/=?^_`{|}~.(),:;<>[\]]*@[a-zA-Z.]+.[a-zA-Z]{2,4}$"
+                               class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2"/>
+                    </div>
+                    <div class="p-2 w-full">
+                        <label class="text-white" for="password">Password:</label>
+                        <input id="password" placeholder="Password" type="password" v-model="form.password"
+                               name="password" required
+                               class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2"/>
+                    </div>
+                    <div class="p-2 w-full">
+                        <label class="text-white" for="confirm_password">Confirm Password:</label>
+                        <input id="confirm_password" name="password_confirmation" placeholder="Confirm Password"
+                               type="password"
+                               class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2"
+                               v-model="form.password_confirmation" required/>
+                    </div>
+                    <div class="p-2 w-full mt-4">
+                        <button type="submit"
+                                class="flex text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                            Register
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import {mapActions, mapGetters} from 'vuex';
 
 export default {
     name: "Register",
-    data () {
+    data() {
         return {
-            form:{
+            form: {
                 name: '',
                 email: '',
                 password: '',
                 password_confirmation: ''
             },
-            errors:[]
+            errors: []
         }
     },
-    methods:{
+    methods: {
         ...mapActions(['registerUser']),
-        async saveForm(){
-            await this.registerUser(this.form).then(()=>{
+        async saveForm() {
+            await this.registerUser(this.form).then(() => {
                 this.$router.push({name: "Login"});
             });
         }
-    },
-    computed: {
-
     },
     watch: {
         // things that vue watches change
@@ -60,6 +74,9 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped="scoped" lang="scss">
+.form-container {
+    width: 100%;
+    transform: translateY(-30%);
+}
 </style>
