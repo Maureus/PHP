efeba3d8993ad017b573e45e4461f630bf51c8c4
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2020-12-04 18:23:10 +0100

    Fix minlength

--- knokledge-db-new/resources/js/components/Register.vue
+++ knokledge-db-new/resources/js/components/Register.vue
@@ -9,12 +9,12 @@
                     <div class="p-2 w-full">
                         <label class="w-full text-white" for="name">Name:</label>
                         <span class="w-full text-red-500" v-if="errors.name">{{ errors.name[0] }}</span>
-                        <input id="name" placeholder="Name" type="text" v-model="form.name" required
+                        <input id="name" placeholder="Name" type="text" v-model="form.name" required minlength="255"
                                class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2"/>
                     </div>
                     <div class="p-2 w-full">
                         <label class="text-white" for="email">Your e-mail:</label>
-                        <input id="email" placeholder="Email" type="email" v-model="form.email" required
+                        <input id="email" placeholder="Email" type="email" v-model="form.email" required minlength="255"
                                pattern="^[a-zA-Z][a-zA-Z0-9!#$%&'*+-/=?^_`{|}~.(),:;<>[\]]*@[a-zA-Z.]+.[a-zA-Z]{2,4}$"
                                class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2"/>
                     </div>
