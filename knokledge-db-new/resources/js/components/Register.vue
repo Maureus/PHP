bd504e5ce984b47df9929bc9427059c30e37354e
Maureus <andriiandrusenko@gmail.com>
2020-12-04 16:41:38 +0100

    Update Register.vue

--- knokledge-db-new/resources/js/components/Register.vue
+++ knokledge-db-new/resources/js/components/Register.vue
@@ -5,7 +5,7 @@
             <div class="p-2 w-full">
                 <label class="w-full text-white" for="name">Name:</label>
                 <span class="w-full text-red-500" v-if="errors.name">{{errors.name[0]}}</span>
-                <input id="name" class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Name" type="text" v-model="form.name" required>
+                <input id="name" class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Name" type="text" v-model="form.name">
             </div>
             <div class="p-2 w-full">
                 <label class="text-white" for="email">Your e-mail:</label>
@@ -20,7 +20,7 @@
                 <input id="confirm_password" class="w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2" placeholder="Confirm Password" type="password" v-model="form.password_confirmation" name="password_confirmation">
             </div>
             <div class="p-2 w-full mt-4">
-                <button @click="saveForm" type="submit" class="flex text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Register</button>
+                <button @click.prevent="saveForm" type="submit" class="flex text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Register</button>
             </div>
         </div>
     </div>
