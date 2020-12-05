8826c3de93b509bc5f52c8b53965572179dd3c7e
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2020-12-05 23:15:06 +0100

    Added all users

--- knokledge-db-new/resources/js/components/UserList.vue
+++ knokledge-db-new/resources/js/components/UserList.vue
@@ -1,30 +1,35 @@
 <template>
-    <div class="flex flex-col">
-        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                    <table class="min-w-full divide-y divide-gray-200">
-                        <thead>
-                        <tr>
-                            <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                                Name
-                            </th>
-                            <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                                Title
-                            </th>
-                            <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                                Status
-                            </th>
-                            <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                                Role
-                            </th>
-                            <th class="px-6 py-3 bg-gray-50"></th>
-                        </tr>
-                        </thead>
-<!--                        v-for-->
-                        <user-list-item></user-list-item>
-                        <user-list-item></user-list-item>
-                    </table>
+    <div>
+        <preloader v-if="loading" class="absolute inset-0 flex items-center justify-center"/>
+        <div v-if="users.length" class="flex flex-col">
+            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead>
+                            <tr>
+                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                    Name
+                                </th>
+                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                    Contact info
+                                </th>
+<!--                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">-->
+<!--                                    Status-->
+<!--                                </th>-->
+                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                    Date of registration
+                                </th>
+                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                    Role
+                                </th>
+                                <th v-if="getUser != null && getUser.role === 'admin'"
+                                    class="px-6 py-3 bg-gray-50"></th>
+                            </tr>
+                            </thead>
+                            <user-list-item v-for="user in users" :key="user.id" :user="user"></user-list-item>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
@@ -33,11 +38,31 @@
 
 <script>
 import UserListItem from "./UserListItem";
+import Preloader from "./Preloader";
+import {mapGetters} from 'vuex';
 
 export default {
     name: "UserList",
     components: {
-        UserListItem
+        UserListItem, Preloader
+    },
+    data() {
+        return {
+            users: [],
+            loading: true
+        }
+    },
+    computed: {
+        ...mapGetters(["getUser"])
+    },
+    async mounted() {
+        await axios.get("http://127.0.0.1:8000/api/users").then(resp => resp.data).then(value => {
+            this.users = value;
+            this.loading = false;
+        });
+        console.log(this.users);
+        console.log(this.getUser);
+        // console.log(this.$store.state.admin); //undef
     }
 }
 </script>
--- knokledge-db-new/resources/js/components/UserListItem.vue
+++ knokledge-db-new/resources/js/components/UserListItem.vue
@@ -4,44 +4,61 @@
         <td class="px-6 py-4 whitespace-no-wrap">
             <div class="flex items-center">
                 <div class="flex-shrink-0 h-10 w-10">
-                    <img class="h-10 w-10 rounded-full"
-                         src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
-                         alt="">
+                    <img class="h-10 w-10 rounded-full" alt=""
+                         src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"/>
                 </div>
                 <div class="ml-4">
-                    <div class="text-sm leading-5 font-medium text-gray-900">
-                        Jane Cooper
-                    </div>
-                    <div class="text-sm leading-5 text-gray-500">
-                        jane.cooper@example.com
-                    </div>
+                    <div class="text-sm leading-5 font-medium text-gray-900">{{ user.name }}</div>
+                    <!--                    <div class="text-sm leading-5 text-gray-500">{{ user.email }}</div>-->
                 </div>
             </div>
         </td>
         <td class="px-6 py-4 whitespace-no-wrap">
-            <div class="text-sm leading-5 text-gray-900">Regional Paradigm Technician</div>
-            <div class="text-sm leading-5 text-gray-500">Optimization</div>
+            <div class="text-sm leading-5 text-gray-500">{{ user.phone == null ? "No phone" : user.phone }}</div>
+            <div class="text-sm leading-5 text-gray-500">{{ user.email == null ? "No email" : user.email }}</div>
         </td>
-        <td class="px-6 py-4 whitespace-no-wrap">
-                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                  Active
-                </span>
+        <!--        <td class="px-6 py-4 whitespace-no-wrap">-->
+        <!--                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">-->
+        <!--                  Active-->
+        <!--                </span>-->
+        <!--        </td>-->
+        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
+            {{ user.created_at | correctDateView }}
         </td>
         <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
-            Admin
+            {{ user.role }}
         </td>
-        <td class="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
+        <td v-if="getUser != null && getUser.role === 'admin'"
+            class="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
             <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
         </td>
     </tr>
-
-    <!-- More rows... -->
     </tbody>
 </template>
 
 <script>
+import {mapGetters} from 'vuex';
+
 export default {
-    name: "UserListItem"
+    name: "UserListItem",
+    computed: {
+        ...mapGetters(["getUser"])
+    },
+    props: {
+        user: {
+            required: true,
+            type: Object
+        }
+    },
+    filters: {
+        correctDateView(value) {
+            if (!value) {
+                return '';
+            }
+            let date = value.split("T").join(" ");
+            return date.split(".")[0];
+        }
+    }
 }
 </script>
 
