952d2ce0ad4321a6dc7a5d6685e49d71612508b1
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2020-12-10 22:26:32 +0100

    Added delete

--- knokledge-db-new/resources/js/components/usersDir/UserList.vue
+++ knokledge-db-new/resources/js/components/usersDir/UserList.vue
@@ -30,7 +30,7 @@
                 </div>
             </div>
         </div>
-        <div v-if="editUser"
+        <div v-if="editUser" style="overflow-y: hidden;"
              class="absolute inset-0 flex items-center justify-center bg-gray-600 bg-opacity-50">
             <div class="flex-column items-center justify-center w-2/5 bg-white border-0 rounded">
                 <h3 class="text-center pt-4 text-lg">Edit profile</h3>
@@ -46,7 +46,7 @@
                         <label for="avatar" class="block text-sm font-medium leading-5 text-gray-700">
                             Choose a new avatar
                         </label>
-                        <input id="avatar" v-model="curUser.avatar" name="avatar"
+                        <input id="avatar" name="avatar" type="file"
                                class="mt-1 form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"/>
                     </div>
                     <div class="col-span-6 sm:col-span-4">
@@ -66,7 +66,7 @@
                     <div class="flex items-center pt-4 justify-start w-full pr-2">
                         <button @click="" type="submit"
                                 class="flex items-center justify-center text-white bg-indigo-500 border-0 py-1 px-2
-                                    focus:outline-none hover:bg-indigo-600 rounded text-xs">
+                                            focus:outline-none hover:bg-indigo-600 rounded text-xs">
                             Confirm
                         </button>
                     </div>
@@ -74,12 +74,12 @@
                 <div class="flex items-center pt-4 justify-end w-full pr-2">
                     <button @click="cancelEditingUserInfo"
                             class="flex items-center justify-center text-white bg-indigo-500 border-0 py-1 px-2
-                                    focus:outline-none hover:bg-indigo-600 rounded text-xs">
+                                            focus:outline-none hover:bg-indigo-600 rounded text-xs">
                         Cancel
                     </button>
                     <button @click="deleteUser"
                             class="flex items-center justify-center text-white bg-red-500 border-0 py-1 px-2
-                                    focus:outline-none hover:bg-red-600 rounded text-xs">
+                                            focus:outline-none hover:bg-red-600 rounded text-xs">
                         Delete
                     </button>
                 </div>
@@ -119,33 +119,34 @@ export default {
     mounted() {
         axios.get("http://127.0.0.1:8000/api/users").then(resp => resp.data).then(value => {
             this.users = value;
-            console.log(value)
+            // console.log(value)
             this.loading = false;
         });
     },
     methods: {
         ...mapActions(["saveErrors"]),
         editUserData(userEditedId) {
+            // console.log(userEditedId);
             this.editUser = true;
             axios.get("http://127.0.0.1:8000/api/users/" + userEditedId)
                 .then(value => value.data)
                 .then(value => {
-                    const user = value;
-                    console.log(user);
-                    this.curUser.name = user.name;
-                    this.curUser.phone = user.phone;
-                    this.curUser.address = user.address;
+                    this.curUser = value;
                 })
                 .catch(error => this.saveErrors(error));
         },
         cancelEditingUserInfo() {
             this.editUser = false;
-            this.curUser.name = "";
-            this.curUser.phone = "";
-            this.curUser.address = "";
+            this.curUser = {};
         },
         deleteUser() {
-
+            console.log(this.curUser.id);
+            axios.delete("http://127.0.0.1:8000/api/users/" + this.curUser.id)
+                .then(() => {
+                    this.users = this.users.filter(user => user.id !== this.curUser.id);
+                    this.editUser = false;
+                    this.curUser = {};
+                });
         },
         saveUserChanges() {
 
@@ -156,6 +157,10 @@ export default {
 
 <style scoped="scoped" lang="scss">
 $margin : 10px;
+
+* {
+    overflow-y : hidden;
+}
 
 table {
     margin-top      : $margin;
@@ -181,6 +186,7 @@ button {
     font-size     : 15px;
     margin-bottom : $margin * 1.5;
     margin-right  : $margin;
-    margin-left   : $margin;
+    //margin-left   : $margin;
+
 }
 </style>
