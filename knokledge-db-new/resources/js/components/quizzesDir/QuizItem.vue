f274d7a677e8b5c36f238ee013a4cbc6aad3be52
Maureus <andriiandrusenko@gmail.com>
2020-12-14 17:33:59 +0100

    upd

--- knokledge-db-new/resources/js/components/quizzesDir/QuizItem.vue
+++ knokledge-db-new/resources/js/components/quizzesDir/QuizItem.vue
@@ -15,7 +15,7 @@
         </td>
         <td v-if="getUser.role === getTeacherRole || getUser.role === getAdminRole">
             <div class="hover-shadow-effect">
-                <button :value="edit"
+                <button @click="setNewItem" :value="edit"
                         class="px-6 py-4 whitespace-no-wrap text-right text-base leading-5 font-medium btn-"
                         data-toggle="modal"
                         data-target="#modalQuiz">
@@ -31,71 +31,6 @@
                 </button>
             </div>
         </td>
-
-        <div class="modal fade" id="modalQuiz" tabindex="-1" role="dialog"
-             aria-labelledby="modalQuiz" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="modalQuizTitle">Edit quiz</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-
-                    <div class="pr-2 pl-2 pt-2">
-                        <div class="col-span-6 sm:col-span-4">
-                            <label class="block text-sm font-medium leading-5 text-gray-700">
-                                Quiz name
-                            </label>
-                            <input v-model="quiz.name" name="name" required
-                                   class="mt-1 form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"/>
-                        </div>
-                        <div class="col-span-6 sm:col-span-4">
-                            <label class="block text-sm font-medium leading-5 text-gray-700">
-                                Date from
-                            </label>
-                            <input name="email" v-model="quiz.date_from" type="date"
-                                   class="mt-1 form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"/>
-                        </div>
-                        <div class="col-span-6 sm:col-span-4">
-                            <label class="block text-sm font-medium leading-5 text-gray-700">
-                                Date till
-                            </label>
-                            <input v-model="quiz.date_till" type="date"
-                                   class="mt-1 form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"/>
-                        </div>
-                        <div class="col-span-6 sm:col-span-4">
-                            <label class="block text-sm font-medium leading-5 text-gray-700">
-                                Description
-                            </label>
-                            <input v-model="quiz.quiz_desc" name="address"
-                                   class="mt-1 form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"/>
-                        </div>
-                        <div class="col-span-6 sm:col-span-4">
-                            <label class="block text-sm font-medium leading-5 text-gray-700">
-                                Number of questions
-                            </label>
-                            <input v-model="quiz.num_questions" name="address"
-                                   class="mt-1 form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"/>
-                        </div>
-                        <div class="btn-container">
-                            <div class="btn-box start">
-                                <button @click="saveQuiz" data-dismiss="modal" class="btn">
-                                    Confirm
-                                </button>
-                            </div>
-                            <div class="btn-box end">
-                                <button data-dismiss="modal" class="btn red">
-                                    Cancel
-                                </button>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
     </tr>
 </template>
 
@@ -127,16 +62,9 @@ export default {
             , "getStudentRole", "getAdminRole", "getTeacherRole", "getQuiz"])
     },
     methods: {
-        ...mapActions(['saveQuiz', 'saveErrors']),
+        ...mapActions(['saveQuiz', 'saveErrors', 'saveQuiz']),
         takeQuiz() {
             this.$router.push({name: 'Quiz', params: {quiz_id: this.quiz.id}});
-        },
-        saveQuiz() {
-            axios.put('http://127.0.0.1:8000/api/quizzes/' + this.quiz.id, this.quiz).then(res => {
-                this.$emit('edit-quiz', this.quiz.id);
-            }).catch(err => {
-                this.saveErrors(err);
-            });
         },
         deleteQuiz() {
             axios.delete('http://127.0.0.1:8000/api/quizzes/' + this.quiz.id).then(res => {
@@ -144,6 +72,12 @@ export default {
             }).catch(err => {
                 this.saveErrors(err);
             });
+        },
+        setNewItem() {
+            let newItem = JSON.parse(JSON.stringify(this.quiz));
+            console.log(newItem);
+            this.saveQuiz(newItem);
+            console.log(this.getQuiz);
         }
     },
     filters: {
@@ -157,68 +91,68 @@ export default {
 </script>
 
 <style scoped="scoped" lang="scss">
-$hoverColor : #dde9f5;
-$margin     : 10px;
+$hoverColor: #dde9f5;
+$margin: 10px;
 
 @import "./resources/sass/hover_effects";
 
 th {
-    color            : white;
-    padding          : 5px 10px;
-    background-color : darken($color : #187fe2, $amount : 3%);
+    color: white;
+    padding: 5px 10px;
+    background-color: darken($color: #187fe2, $amount: 3%);
 }
 
 td {
-    padding : 5px 10px;
+    padding: 5px 10px;
 }
 
 button {
     &:focus {
-        outline : none;
+        outline: none;
     }
 }
 
 .btn-container {
-    display : flex;
+    display: flex;
 }
 
 .btn-box {
-    padding-top : 50px;
+    padding-top: 50px;
 
     &.start {
-        text-align : start;
-        width      : 80%;
+        text-align: start;
+        width: 80%;
     }
 
     &.end {
-        text-align   : end;
-        margin-right : 0.5rem;
-        width        : 20%;
+        text-align: end;
+        margin-right: 0.5rem;
+        width: 20%;
     }
 }
 
 .btn {
-    width            : 100px;
-    height           : auto;
-    font-size        : 14px;
-    margin-bottom    : $margin * 1.5;
-    color            : white;
-    background-color : #6875f5;
+    width: 100px;
+    height: auto;
+    font-size: 14px;
+    margin-bottom: $margin * 1.5;
+    color: white;
+    background-color: #6875f5;
 
     &.red {
-        background-color : #f05252;
+        background-color: #f05252;
 
         &:hover {
-            background-color : #e02424;
+            background-color: #e02424;
         }
     }
 
     &:hover {
-        background-color : #5850ec;
+        background-color: #5850ec;
     }
 
     &:focus {
-        outline : none;
+        outline: none;
     }
 }
 </style>
