1673ed702a59ad781c4e1159232b61d50edc1a3f
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2020-12-21 15:06:23 +0100

    Added about page

--- knokledge-db-new/resources/js/components/About.vue
+++ knokledge-db-new/resources/js/components/About.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="text-2xl text-white">
-        About page
+    <div class="about-container">
+        <div class="about">
+            This is a web application was made by Nikita Yarosh and Andrii Andrusenko how as a semester work for IDAS2.
+        </div>
     </div>
 </template>
 
@@ -11,5 +13,18 @@ export default {
 </script>
 
 <style scoped>
+.about-container {
+    display         : flex;
+    align-items     : center;
+    justify-content : center;
+    height          : 80vh;
+}
 
+.about {
+    color      : white;
+    width      : 60%;
+    margin     : auto;
+    text-align : center;
+    font-size  : 2.5em;
+}
 </style>
