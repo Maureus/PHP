38631a2333a860b819f20e77c217d84b7bd8e15b
Maureus <andriiandrusenko@gmail.com>
2021-01-13 17:08:46 +0100

    upd question insert

--- knokledge-db-new/app/Http/Controllers/API/QuestionController.php
+++ knokledge-db-new/app/Http/Controllers/API/QuestionController.php
@@ -14,19 +14,17 @@ class QuestionController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
-    {
+    public function index(): \Illuminate\Http\JsonResponse {
         return response()->json(Question::selectAllQuestions());
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request): \Illuminate\Http\JsonResponse {
         $validation = Validator::make($request->all(), [
             'name' => 'required|max:255',
             'answer_1' => 'required|max:255',
@@ -51,23 +49,21 @@ class QuestionController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\JsonResponse
      */
-    public function show($id)
-    {
+    public function show($id): \Illuminate\Http\JsonResponse {
         return response()->json(Question::selectQuestion($id));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\JsonResponse
      */
-    public function update(Request $request, $id)
-    {
+    public function update(Request $request, $id): \Illuminate\Http\JsonResponse {
         $validation = Validator::make($request->all(), [
             'name' => 'required|max:255',
             'answer_1' => 'required|max:255',
@@ -92,11 +88,10 @@ class QuestionController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\JsonResponse
      */
-    public function destroy($id)
-    {
+    public function destroy($id): \Illuminate\Http\JsonResponse {
         try {
             Question::deleteQuestion($id);
         } catch (\Exception $ex) {
